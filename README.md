51de7ea8b4b00f24320470f270e545be0039b8ed
EdSaleh <ed.saleh@outlook.com>
2023-06-16 18:00:38 -0400

    feat: This is a microservice built with Express in Node.js for managing products with CRUD operations (Create, Read, Update, Delete) for product items with SQLite as Database.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # express-crud-microservice
 This is a microservice built with Express in Node.js for managing products with CRUD operations (Create, Read, Update, Delete) for product items with SQLite as Database.
+
+## Installation
+
+1. Install dependencies:
+   ```
+   npm install
+   ```
+
+2. Start the server:
+   ```
+   npm start
+   ```
+
+4. The server will start running on `http://localhost:3000`.
+
+## API Documentation
+
+The API is documented using Swagger/OpenAPI Specification. You can access the API documentation at `http://localhost:3000/api-docs` when the server is running.
+
+## Endpoints
+
+- **POST /products**: Create a new product.
+- **GET /products/:id**: Get a product by ID.
+- **PUT /products/:id**: Update a product by ID.
+- **DELETE /products/:id**: Delete a product by ID.
+
+## Testing
+
+```
+npm test
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ server/tests/product.test.js
@@ -0,0 +1,80 @@
+const request = require('supertest');
+const fs = require('fs');
+const path = require('path');
+const app = require('../index');
+
+
+const databasePath = path.join(__dirname, '..', 'database.sqlite');
+
+function waitForDatabase() {
+  return new Promise((resolve, reject) => {
+    const timeout = 5000; // Adjust the timeout as needed
+    const interval = 200; // Adjust the interval as needed
+    let elapsedTime = 0;
+
+    const checkDatabase = setInterval(() => {
+      if (fs.existsSync(databasePath)) {
+        clearInterval(checkDatabase);
+        resolve();
+      } else {
+        elapsedTime += interval;
+        if (elapsedTime >= timeout) {
+          clearInterval(checkDatabase);
+          reject(new Error('Timeout: Database file not found'));
+        }
+        console.log('Waiting for database file to be created...');
+      }
+    }, interval);
+  });
+}
+
+describe('Product API', () => {
+
+  // Run this before all tests
+  beforeAll(async () => {
+    // Perform any setup tasks, such as waiting for the database file to be created
+    await waitForDatabase();
+  });
+
+  // Test for creating a new product
+  test('POST /products', async () => {
+    const response = await request(app)
+      .post('/products')
+      .send({ name: 'Sample Product', price: 10 });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.name).toBe('Sample Product');
+    expect(response.body.price).toBe(10);
+  });
+
+  // Test for getting a product by ID
+  test('GET /products/:id', async () => {
+    const response = await request(app).get('/products/1');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('name');
+    expect(response.body).toHaveProperty('price');
+  });
+
+  // Test for updating a product by ID
+  test('PUT /products/:id', async () => {
+    const response = await request(app)
+      .put('/products/1')
+      .send({ name: 'Updated Product', price: 20 });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe('Updated Product');
+    expect(response.body.price).toBe(20);
+  });
+
+  // Test for deleting a product by ID
+  test('DELETE /products/:id', async () => {
+    const response = await request(app).delete('/products/1');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toBe('Product deleted successfully');
+  });
+});
